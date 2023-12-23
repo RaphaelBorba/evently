0cbe24b958eb980dd5ada8f1daf313553fac59bc
Raphael Borba <raphaelbsx@gmail.com>
2023-12-23 11:54:00 -0300

    feat: header finish

--- src/components/shared/Navitems.tsx
+++ src/components/shared/Navitems.tsx
@@ -1,10 +1,29 @@
+"use client"
+import { headerLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 
 export default function Navitems(){
 
+    const pathname = usePathname()
+
     return(
-        <nav>
-            asdasd
-        </nav>
+        <ul className="md:flex-between flex w-full flex-col items-start gap-5 md:flex-row">
+            {headerLinks.map((link) =>{
+                const isActive = pathname === link.route
+                return(
+                    <li 
+                    className={cn("flex-center p-medium-16 whitespace-nowrap",{
+                        'text-primary-500':isActive
+                    })}
+                    key={link.route}
+                    >
+                        <Link href={link.route}>{link.label}</Link>
+                    </li>
+                )
+            })}
+        </ul>
     )
 }
