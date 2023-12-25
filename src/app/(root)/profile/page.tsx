a95b310853fc2c4752cc5e5dec88bacf1927a5f8
Raphael Borba <raphaelbsx@gmail.com>
2023-12-25 16:30:18 -0300

    feat: show orders events in profile page

--- src/app/(root)/profile/page.tsx
+++ src/app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import Collection from "@/components/shared/Collection";
 import { Button } from "@/components/ui/button";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
 import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
@@ -10,11 +12,14 @@ export default async function Page() {
 
     const {sessionClaims} = auth()
     const userId = sessionClaims?.userId as string
-
+    
     const organizedEvents = await getEventsByUser({
         userId,
         page:1
     })
+
+    const orders = await getOrdersByUser({userId, page:1})
+    const orderedEvents = orders?.data.map((order:IOrder)=>order.event) || []
 
     return (
         <>
@@ -26,9 +31,9 @@ export default async function Page() {
                     </Button>
                 </div>
             </section>
-            {/*  <section className="wrapper my-8">
+             <section className="wrapper my-8">
             <Collection
-                    data={[]}
+                    data={orderedEvents}
                     emptyTitle="No Events Tickets Purchase"
                     emptyStateSubtext="No worries - plenty of exciting events to explore"
                     collectionType="My_Tickets"
@@ -37,7 +42,7 @@ export default async function Page() {
                     urlParamName={"ordersPage"}
                     totalPages={2}
                 />
-            </section> */}
+            </section>
             <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
                 <div className="wrapper flex items-center justify-center sm:justify-between">
                     <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
