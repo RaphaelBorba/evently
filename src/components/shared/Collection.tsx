d669cbe70e5c0da23eef4eec65d49e0e710d03aa
Raphael Borba <raphaelbsx@gmail.com>
2023-12-25 19:40:07 -0300

    feat: add pagination to collections

--- src/components/shared/Collection.tsx
+++ src/components/shared/Collection.tsx
@@ -1,15 +1,16 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import Card from "./Card";
+import Pagination from "./Pagination";
 
 type CollectionProps = {
     data: IEvent[];
     emptyTitle: string;
     emptyStateSubtext: string;
-    collectionType: "Events_Organized" | "My_Tickets" | "All_Events";
     limit: number;
     page: number | string;
     totalPages?: number;
-    urlParamName?: string
+    urlParamName?: string;
+    collectionType?: "Events_Organized" | "My_Tickets" | "All_Events";
 }
 
 
@@ -42,6 +43,13 @@ export default function Collection({
                         )
                     })}
                 </ul>
+                {totalPages > 1 && (
+                    <Pagination
+                    urlParamName={urlParamName}
+                    page={page}
+                    totalPages={totalPages}
+                    />
+                )}
             </div>
         ):
         (
